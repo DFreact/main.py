4cc6e312fc0aa912158a41f2e68ab1ce2ae97e25
Dany <Danich2014russ@gmail.com>
2024-07-27 19:50:30 +0300

    2023/10/02 00:00|Дополнительное практическое задание по модулю* Код + палиндром

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-
-
 def password(number):
     result = []
     for i in range(1, number):
         for j in range(1,number):
             if number % (i+j) == 0 and i != j:
-                num = str(i) + str(j)
+                num = str(i) + "|" + str(j)
                 result.append(num)
 
-    for i_p in result:
-        for j_p in result:
+    new_result = []
+    for m in result:
+        new_result.append(m.split("|"))
+
+    for i_p in new_result:
+        for j_p in new_result:
             if i_p == j_p:
                 continue
             elif i_p == j_p[::-1]:
-                result.remove(j_p)
 
-    return result
+                new_result.remove(j_p)
+
+    new_list = ""
+    for i in new_result:
+        new_list +=  ",".join(str(element) for element in i) + " | "
+
+
+    return(new_list)
 
 data = [x for x in range(3,21)]
 
-
 for i in data:
 
-    print(i, password(i))
+    print(f'Для числа {i} подходит пароль: {password(i)}')
+
