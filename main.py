14c5a9fa0e244a313b34b9f1f546bb8f2e7a696a
Dany <Danich2014russ@gmail.com>
2024-07-28 16:06:48 +0300

    2023/10/07 00:00|Самостоятельная работа по уроку "Распаковка позиционных параметров".

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    ends_list = [".com", ".ru", ".net"]
-    recipient_ends = True
-    sender_ends = True
-    if "@" not in recipient or "@" not in sender:
-        return print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+def print_params(a = 1, b="строка", c = True):
+    print(a, b, c)
 
+print_params(1,2,3)
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
 
-    for i in ends_list:
-        if recipient.endswith(i):
-            recipient_ends = False
+value_list = [{"a": 1, "b": 2}, "stroka", [15, 20]]
+values_dict = {"a": 12, "b": [100, 250], "c": "nestroka"}
 
-        if sender.endswith(i):
-            sender_ends = False
+print_params(*value_list)
+print_params(**values_dict)
 
-
-    if recipient_ends or sender_ends:
-        return print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
-    elif recipient == sender:
-        return print(f"Нельзя отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-    else:
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+values_list_2 = [1, "perec"]
+print_params(*values_list_2, 42)
