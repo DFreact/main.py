7fe37f6ddd9d99d980aecd1fd4b3a549c5d443d3
Dany <Danich2014russ@gmail.com>
2024-07-28 19:33:18 +0300

    2023/10/12 00:00|Дополнительное практическое задание по модулю*

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-def get_multiplied_digits(number):
-    str_number = str(number)
-    first = int(str_number[0])
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
-    else:
-        return first
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
 
-result = get_multiplied_digits(40203)
+
+def calculate_structure_sum(data_structure):
+    total_sum = 0
+    if isinstance(data_structure, int):
+        return data_structure
+    for elem in data_structure:
+
+        if isinstance(elem, tuple):
+            total_sum += calculate_structure_sum(elem)
+        elif isinstance(elem, list):
+            total_sum += calculate_structure_sum(elem)
+        elif isinstance(elem, dict):
+            key, value = elem.keys(), elem.values()
+            total_sum += calculate_structure_sum(key)
+            total_sum += calculate_structure_sum(value)
+        elif isinstance(elem, int):
+            total_sum += elem
+        elif isinstance(elem, str):
+            total_sum += len(elem)
+        else:
+            total_sum += calculate_structure_sum(elem)
+    return total_sum
+
+
+result = calculate_structure_sum(data_structure)
 print(result)
-
