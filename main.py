b8ff2b14d55f9a227762cc6fb2bf103074518a70
Dany <Danich2014russ@gmail.com>
2024-07-30 21:28:27 +0300

    2023/10/04 00:00|Домашняя работа по уроку "Пространство имён"

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    ends_list = [".com", ".ru", ".net"]
-    recipient_ends = True
-    sender_ends = True
-    if "@" not in recipient or "@" not in sender:
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+calls = 0
 
 
-    for i in ends_list:
-        if recipient.endswith(i):
-            recipient_ends = False
-
-        if sender.endswith(i):
-            sender_ends = False
+def count_calls():
+    global calls
+    calls += 1
 
 
-    if recipient_ends or sender_ends:
-        return print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
-    elif recipient == sender:
-        return print(f"Нельзя отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-    else:
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+def is_contains(string, list_):
+    count_calls()
+    return string.lower() in [word.lower() for word in list_]
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
