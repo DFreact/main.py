f9dfdac3ee8104d08058c77abd6debb59072b74a
Dany <Danich2014russ@gmail.com>
2024-08-10 22:36:30 +0300

    2023/10/25 00:00|Домашняя работа по уроку "Атрибуты и методы объекта."

--- module_4_1.py
+++ module_4_1.py
@@ -1,5 +1,18 @@
-from fake_math import divide as fake_math
-from true_math import divide as true_math
+class House():
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
 
-print(fake_math(10, 0))
-print(true_math(10, 0))
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            floor_list = [int(x) for x in range(1, new_floor+1)]
+            for i in floor_list:
+                print(i)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
